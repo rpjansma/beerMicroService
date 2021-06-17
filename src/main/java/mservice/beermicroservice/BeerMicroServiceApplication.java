5e9d63b9b1245724ba50c1d03ec325b0f56c42ad
Raul Jansma Paes <rpjansma@gmail.com>
2021-06-16 21:19:53 -0300

    Micro service for beer project kickstart.

---
+++ src/main/java/mservice/beermicroservice/BeerMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package mservice.beermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerMicroServiceApplication.class, args);
+    }
+
+}
