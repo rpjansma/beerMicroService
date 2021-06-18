0d75737e76ca7a5066102e9f373286dfb6aadab8
Raul Jansma Paes <rpjansma@gmail.com>
2021-06-18 11:54:50 -0300

    Controller tests finished.

--- src/test/java/mservice/beermicroservice/web/controller/BeerControllerTest.java
+++ src/test/java/mservice/beermicroservice/web/controller/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package mservice.beermicroservice.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mservice.beermicroservice.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -17,6 +19,9 @@ class BeerControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     void getBeerById() throws Exception {
@@ -27,10 +32,24 @@ class BeerControllerTest {
     }
 
     @Test
-    void saveNewBeer() {
+    void saveNewBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(post("/api/v1/beer/")
+            .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
     }
 
     @Test
-    void updateBeerById() {
+    void updateBeerById() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isNoContent());
     }
 }
